14165df97a7fcc5f2b5fd74f55fde405096b8bb1
Tamashiro syohei <e175752@ie.u-ryukyu.ac.jp>
2017-11-24 23:41:54 +0900

    prog2_report4

--- src/Prog2.java
+++ src/Prog2.java
@@ -1,6 +1,13 @@
 public class Prog2 {
-    public static void main(String[] args){
-        String str = null;
-        System.out.println(str.length());
+    public static void main(String[] args) {
+        try {
+            String str = null;
+            System.out.println(str.length());
+        }
+        catch(NullPointerException e) {
+            System.out.println(e);
+            System.out.println(e.getMessage());
+
+        }
     }
 }
